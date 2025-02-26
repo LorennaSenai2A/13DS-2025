cd8d9d7c99957fc60760b6a0950884e98cbdb9e1
Aluna.Lorena <lorennasenaia@gmail.com>
2025-02-26 16:17:44 -0300

    Mudanças do prof

--- 03-devlinks/src/App.jsx
+++ 03-devlinks/src/App.jsx
@@ -2,65 +2,68 @@ import "./App.css";
 import fotoDark from "./img/chococatColor.jpg";
 import fotoLight from "./img/myMellodyColor.jpg";
 
-
-import Perfil from "./components/perfil/perfil"
-import Switch from "./components/switch/Switch"
-import Links from "./components/links/Links"
+import Perfil from "./components/perfil/perfil";
+import Switch from "./components/switch/Switch";
+import Links from "./components/links/Links";
 import SocialLinks from "./components/socialLinks/SocialLinks";
 import Rodape from "./components/rodape/Rodape";
 import { useState } from "react";
 
 const App = () => {
-const [isLight,setIsLight] = useState(true);
+  const [isLight, setIsLight] = useState(true);
 
-const troca = () => {
-  setIsLight(!isLight);
-}
+  const troca = () => {
+    setIsLight(!isLight);
+  };
 
   return (
-<div id="App" className={isLight ? "light" : ""}>
+    <div id="App" className={isLight ? "light" : ""}>
+      <div id="container">
+        <Perfil fotoPerfil={isLight ? fotoLight : fotoDark}>
+          @ElLorennaXD
+        </Perfil>
 
-<div id="container">
+        <Switch troca={troca} isLight={isLight} />
 
-   <Perfil fotoPerfil={isLight ? fotoLight : fotoDark }>
-    @ElLorennaXD</Perfil>
+        <div id="links">
+          <ul>
+            <Links links={"https://github.com/LorennaSenai2A"}>GitHub</Links>
+            <Links
+              links={"https://www.instagram.com/lorenazanon.dix/?next=%2F"}
+            >
+              Instagram
+            </Links>
+            <Links
+              links={
+                "https://www.figma.com/design/ePEmC8G8cGb1xnF7p84xAY/Primeiro-App?node-id=0-1&p=f&t=mbr5fodB3azEoQN6-0"
+              }
+            >
+              Figma
+            </Links>
+            <Links links={"https://br.pinterest.com/loreninhazanon/"}>
+              Pinterest
+            </Links>
+          </ul>
+        </div>
 
-   <Switch troca={troca} isLight={isLight}/>
-
- <div id="links">
-
-   <ul>
-   <Links links={"https://github.com/LorennaSenai2A"}>GitHub</Links>
-   <Links links={"https://www.instagram.com/lorenazanon.dix/?next=%2F"}>Instagram</Links>
-   <Links links={"https://www.figma.com/design/ePEmC8G8cGb1xnF7p84xAY/Primeiro-App?node-id=0-1&p=f&t=mbr5fodB3azEoQN6-0"}>Figma</Links>
-   <Links links={"https://br.pinterest.com/loreninhazanon/"}>Pinterest</Links>
-   </ul>
-</div>
-</div>
-
-<div id="socialLinks">
-  <SocialLinks 
-  link={"https://github.com/"}
-  icon={"logo-github"}
-  />
-  <SocialLinks
-    link={"https://www.instagram.com/"}
-    icon={"logo-instagram"}
-  />
-  <SocialLinks 
-    link={"https://www.figma.com/login"}
-    icon={"logo-figma"}
-  />
-  <SocialLinks 
-    link={"https://br.pinterest.com/"}
-    icon={"logo-pinterest"}
-  /> 
-
-</div>
-<Rodape>
-  Lorenna
-</Rodape>
-   </div>
+        <div id="socialLinks">
+          <SocialLinks link={"https://github.com/"} icon={"logo-github"} />
+          <SocialLinks
+            link={"https://www.instagram.com/"}
+            icon={"logo-instagram"}
+          />
+          <SocialLinks
+            link={"https://www.figma.com/login"}
+            icon={"logo-figma"}
+          />
+          <SocialLinks
+            link={"https://br.pinterest.com/"}
+            icon={"logo-pinterest"}
+          />
+        </div>
+        <Rodape>Lorenna</Rodape>
+      </div>
+    </div>
   );
 };
 
