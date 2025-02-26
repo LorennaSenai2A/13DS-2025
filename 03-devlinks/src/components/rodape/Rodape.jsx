2439c21a9a7ab14865fff17b8fc9073b75e4984c
Aluna.Lorena <lorennasenaia@gmail.com>
2025-02-26 08:03:53 -0300

    Inicio de Repositorio - React

---
+++ 03-devlinks/src/components/rodape/Rodape.jsx
@@ -0,0 +1,13 @@
+import styles from './Rodape.module.css'
+
+const Rodape = () => {
+  return (
+   <footer>
+    <p>
+        Feito com 💜 por <a href="https://github.com">Lorenna</a>
+    </p>
+   </footer>
+  )
+}
+
+export default Rodape;
---
+++ 03-devlinks/src/components/socialLinks/SocialLinks.jsx
@@ -0,0 +1,9 @@
+import styles from 'react'
+
+const SocialLinks = () => {
+  return (
+  <a href="https://google.com">🌎</a>
+  )
+}
+
+export default SocialLinks;
