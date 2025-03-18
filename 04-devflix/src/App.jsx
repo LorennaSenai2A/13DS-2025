ae9b4a5970400feb9a2519f04f3ff5e92b325d39
Aluna.Lorena <lorennasenaia@gmail.com>
2025-03-18 09:28:29 -0300

    Ajuste do App.jsx, MovieCard.jsx e MovieDescription

--- 04-devflix/src/App.jsx
+++ 04-devflix/src/App.jsx
@@ -54,7 +54,7 @@ e.key === "Enter" && searchMovies(search);
 {movies?.length > 0 ? (
   <div className="container">
 {movies.map((movie, index) => (
-<MovieCard key={index} {...movie} />
+<MovieCard key={index} apiUrl={apiUrl} {...movie} />
   ))}
   </div>
 ) : (
