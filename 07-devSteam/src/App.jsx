2f18fb1a2e7cf62e7546c03e7c0c57d94eef02a8
Aluna.Lorena <lorennasenaia@gmail.com>
2025-04-23 09:00:13 -0300

    ajustes no promoCard e Promotion

--- 07-devSteam/src/App.jsx
+++ 07-devSteam/src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import './App.css';
-import Header from './components/Header';
-import Promotion from './components/Promotion';
+import { useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Promotion from "./components/Promotion";
 
 function App() {
- // const [carrinhoItem, setCarrinhoItem] = useState([]);
- //const [mostraCarrinho, setMostraCarrinho] = useState(false) ; 
+  const [contador, setContador] = useState(0);
 
-//const adicionarCarrinho = (produto) => {
-//setCarrinhoItem((produtosAnteriores) => {
-  //const existing = produtosAnteriores.find(
-    //(item) => item.id === produto.id);
-    //if (existing) {
-  //return produtosAnteriores.map((item) => {
-    //item.id === produto.id
-      //? { ...item, quantidade: item.quantidade + 1 }
-      //: item;
-//});
-//};
-
-const [contador, setContador] = useState(0);
-
-const handleAddCarrinho = () => {
-setContador(contador + 1);
-}
+  const handleAddCarrinho = () => {
+    setContador(contador + 1);
+  };
 
   return (
     <>
-        <Header contadorJogos={contador} />
-        <Promotion />
-        <button onClick={handleAddCarrinho}>+1</button>
+      <Header contadorJogos={contador} />
+      <Promotion
+        onAddCarrinho={handleAddCarrinho} //adicionando o click para promoção
+      />
     </>
   );
 }
--- 07-devSteam/src/components/Header.jsx
+++ 07-devSteam/src/components/Header.jsx
@@ -2,27 +2,32 @@ import React from "react";
 
 const Header = (props) => {
   return (
-    <header className="w-100 navbar navbar-dark bg-dark justify-content-around align-items-center">
-      <div id="logo-pesq" className="d-flex gap-5 w-50 justify-content-beetween">
-        <div id="logo" role="button" className="d-flex align-items-center">
+    <header className="pt-4 w-100 navbar navbar-dark bg-dark justify-content-around align-items-center">
+      <div id="info" className="d-flex gap-5 w-50 justify-content-between">
+        <div id="logo" role="button" className="d-flex align-items-center me-5">
           <i className="bi bi-controller fs-1 text-light me-3"></i>
           <span className="navbar-brand fw-bold fs-3">DevSteam</span>
         </div>
 
         <input
-        type="text"
-        className="w-100 d-none d-md-block border-0 rounded-1 buscar px-4 my-2"
-        placeholder="Buscar..." 
+          type="text"
+          className="w-100 d-none d-md-block border-0 rounded-1 buscar px-4 my-2 ms-5"
+          placeholder="Buscar..."
         />
-        </div>
-      
-      <div id="carrinho" role="button" className="position">
+      </div>
+
+      <div
+        id="carrinho"
+        role="button"
+        className="position-relative d-flex align-items-center gap-3"
+      >
         <i className="bi bi-cart4 text-light fs-2"></i>
 
         {props.contadorJogos > 0 && (
-          <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger"></span>
+          <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+            {props.contadorJogos}
+          </span>
         )}
-
       </div>
     </header>
   );
