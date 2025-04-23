e37ef6ed94f8b8ee2442a9a76d3ad91e1a6ae8e5
Aluna.Lorena <lorennasenaia@gmail.com>
2025-04-23 16:26:23 -0300

    ajustes no header e login, e também no carrinho e checkout

--- 07-devSteam/src/components/CarrinhoOffCanvas.jsx
+++ 07-devSteam/src/components/CarrinhoOffCanvas.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import { useNavigate } from "react-router";
 
 const CarrinhoOffCanvas = (props) => {
+const navigate = useNavigate();
+
   const total = props.carrinhoItem.reduce(
     (acc, item) => 
       acc + (item.preco - (item.preco * item.desconto) / 100) * item.quantidade,
     0
   );
+
+const goToCheckout = () => {
+
+  navigate("/checkout");
+}
 
   return (
     <div
@@ -101,7 +109,14 @@ const CarrinhoOffCanvas = (props) => {
               <strong>Total:</strong>
               <strong>R$ {total.toFixed(2)}</strong>
             </div>
-            <button className="btn desconto w-100 mt-2 fs-5 ">Finalizar compra</button>
+            <button 
+            id="addCarrinho"
+            className="btn desconto w-100 mt-2 fs-5"
+            data-bs-toggle="offcanvas"
+            data-bs-target="#carrinhoOffCanvas"
+            onClick={goToCheckout}
+            >
+              Finalizar compra</button>
           </>
         )}
       </div>
